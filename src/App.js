6c0ea3d58d6d430f96d8576a3857f18c98150a9c
ParveshRao <102420001+ParveshRao@users.noreply.github.com>
2022-03-31 09:17:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+function Sidebar() {
+    return (
+        <div  className="Sidebar">
+        <h1>Sidebar</h1>         
+          </div>
+          
+       );
+    
+    }
+export default Sidebar;
